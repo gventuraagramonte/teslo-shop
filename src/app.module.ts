9e504bfefa53d2cd41009abbea8f1ce87355b0f5
Giorgio Ventura <gventuraagramonte@gmail.com>
2022-11-26 23:50:04 -0500

    modify column in the database and relational new entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     password: process.env.DB_PASSWORD,
     autoLoadEntities: true,
     synchronize: true, // No usar en prod
-  })],
+  }),
+  ProductsModule],
 })
 export class AppModule {}
