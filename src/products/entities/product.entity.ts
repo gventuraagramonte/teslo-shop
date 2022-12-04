4dfaa88e0d899a40eb03d7a1f11b18f3a21f63b3
Giorgio Ventura <gventuraagramonte@gmail.com>
2022-12-04 08:37:33 -0500

    Relation OneToMany and ManyToOne

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from './product-image.entity';
 
 // This is a table
 @Entity()
@@ -36,6 +37,13 @@ export class Product {
     tags: string[]
 
     //Images
+    // Un producto puede tener muchas imagenes
+    @OneToMany(
+        ()=> ProductImage,
+        (productImage) => productImage.product,
+        {cascade: true}
+    )
+    images?: ProductImage
 
 
     @BeforeInsert()
